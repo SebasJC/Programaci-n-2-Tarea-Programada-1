c2369b1a03277e86dc2e24880d793c9d0e8ed286
SebasJC <47463760+SebasJC@users.noreply.github.com>
2022-05-03 21:49:02 -0600

    Se terminó de programar el archivo envioPostal.cpp

--- Programaci-n-2-Tarea-Programada-1/src/envioPostal.cpp
+++ Programaci-n-2-Tarea-Programada-1/src/envioPostal.cpp
@@ -12,12 +12,66 @@ float EnvioPostal::CalculoPago()
 {
     if (this->clase == "primera")
     {
-        if (this->kilometros > 0 && this->kilometros <= 3)
+        if (this->kilometros >= 0 && this->kilometros <= 3)
         {
             this->costoPorKilometraje = 0.300;
             return this->costoPorKilometraje;
         }
+        else
+        {
+            if (this->kilometros >= 4 && this->kilometros <= 8)
+            {
+                this->costoPorKilometraje = 0.450;
+                return this->costoPorKilometraje;
+            }
+            else
+            {
+                if (this->kilometros > 0 && this->kilometros <= 3)
+                {
+                    this->costoPorKilometraje = 0.600;
+                    return this->costoPorKilometraje;
+                }
+            }
+            
+        }
+        
     }
+
+    if (this->clase == "segunda")
+    {
+        if (this->kilometros >= 0 && this->kilometros <= 3)
+        {
+            this->costoPorKilometraje = 0.0280;
+            return this->costoPorKilometraje;
+        }
+        else
+        {
+            if (this->kilometros >= 4 && this->kilometros <= 8)
+            {
+                this->costoPorKilometraje = 0.0530;
+                return this->costoPorKilometraje;
+            }
+            else
+            {
+                if (this->kilometros > 0 && this->kilometros <= 3)
+                {
+                    this->costoPorKilometraje = 0.0750;
+                    return this->costoPorKilometraje;
+                }
+            }
+            
+        }
+        
+    }
+
+    if (this->clase == "tercera")
+    {
+        this->costoPorKilometraje = 0.0120;
+        return this->costoPorKilometraje;
+    }
+    
+    
+    
     
 }
 
